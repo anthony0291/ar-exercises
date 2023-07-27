5fc9b8c83334d5e1eb3ad36fd866049bd36d2259
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-07-27 03:13:00 +0000

    finished exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,8 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find(3) #create @store3 variable and add Store with id 3
+@store3.destroy
+
+puts "----------"
+puts "Store count is: #{Store.count}"
