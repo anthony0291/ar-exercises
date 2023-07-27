f6ac132f56821cbd3cad191f8d6342ef9519324e
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-07-27 04:28:46 +0000

    added exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+#.sum method
+@total_revenue = Store.sum(:annual_revenue)
+
+# @average_reven = @total_revenue / Store.count
+#use .average method
+@average_revenue = Store.average(:annual_revenue)
+
+@high_performers = Store.where("annual_revenue > ?", 1000000).count
+
+puts "Total Revenue: #{@total_revenue}"
+puts "Average Revenue: #{@average_revenue}"
+puts "High-Performers: #{@high_performers}"
