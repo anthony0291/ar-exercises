dfc723a2fece4a34ff151c2e2830244a5facc74f
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-07-27 03:10:41 +0000

    finished exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1) #.find the id number
+@store2 = Store.find(2)
+@store1.update(name: 'Target') #used to .update store namename
+
+puts "Store 1: #{@store1.name}"
